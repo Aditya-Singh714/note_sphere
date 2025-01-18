57a5aefd7d88d3513e1ab06de30cf0b8254d5ff0
Aditya-Singh714 <adityajaat0000@gmail.com>
2025-01-18 12:37:58 +0530

    Font family & Headings tools

---
+++ src/app/documents/[documentId]/fontFamily.tsx
@@ -0,0 +1,62 @@
+import { useEditorStore } from "@/store/use-editor-store";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDownIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+export const FontFamilyButton = () => {
+    const { editor } = useEditorStore();
+
+    const fonts = [
+        { label: "Arial", value: "Arial" },
+        { label: "Verdana", value: "Verdana" },
+        { label: "Helvetica", value: "Helvetica" },
+        { label: "Times New Roman", value: "Times New Roman" },
+        { label: "Georgia", value: "Georgia" },
+        { label: "Courier New", value: "Courier New" },
+        { label: "Open Sans", value: "Open Sans" },
+        { label: "Montserrat", value: "Montserrat" },
+        { label: "Inter", value: "Inter" },
+        { label: "Comic Sans", value: "Comic Sans MS, Comic Sans" },
+        { label: "Serif", value: "Serif" },
+        { label: "Monospace", value: "Monospace" },
+        { label: "Cursive", value: "Cursive" },
+        { label: "CSS Variable", value: "var(--font-family)" },
+        { label: "Exo 2", value: "Exo 2" },
+        { label: "Unset Font Family", value: "unset" }
+    ];
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                    className=
+                    "h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <span className="truncate">
+                        {editor?.getAttributes("textStyle").FontFamily || "Arial"}
+                    </span>
+                    <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {fonts.map(({ label, value }) => (
+                    <button
+                        onClick={() => editor?.chain().focus().setFontFamily(value).run()}
+                        key={value}
+                        className={cn(
+                            "flex items-center gap-x-2 px-2 rounded-sm hover:bg-neutral-200/80", editor?.getAttributes("textStyle").FontFamily === value && "bg-neutral-200/80"
+                        )}
+                        style={{ fontFamily: value }}
+                    >
+                        <span className="text-sm">{label}</span>
+                    </button>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
