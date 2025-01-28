9d168aa17336418441f91879b086e623eead5fde
Aditya-Singh714 <adityajaat0000@gmail.com>
2025-01-28 12:32:40 +0530

    Authentication setup (Clerk)

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { UserButton } from "@clerk/nextjs"
 import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
@@ -12,6 +13,7 @@ export const Navbar = () => {
                 <h3 className="text-xl">NoteSphere</h3>
             </div>
             <SearchInput />
+            <UserButton />
             <div/>
         </nav>
     );
