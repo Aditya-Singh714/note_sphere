4d685253f0b1d72b7faffe0ed195064a4a6491e4
Aditya-Singh714 <adityajaat0000@gmail.com>
2025-02-22 12:35:46 +0530

    Collaborative margin ruler

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,11 +1,20 @@
 import { useRef, useState } from "react";
 import { FaCaretDown } from "react-icons/fa";
+import { useStorage, useMutation } from "@liveblocks/react";
 
 const markers = Array.from({ length: 83 }, (_, i) => i);
 
 export const Ruler = () => {
-    const [leftMargin, setLeftMargin] = useState(566);
-    const [rightMargin, setRightMargin] = useState(56);
+    const leftMargin = useStorage((root) => root.leftMargin) ?? 56;
+    const setLeftMargin = useMutation(({ storage}, position: number) => {
+        storage.set("leftMargin", position);
+    },[]);
+
+    const rightMargin = useStorage((root) => root.rightMargin) ?? 56;
+    const setRightMargin = useMutation(({ storage}, position: number) => {
+        storage.set("rightMargin", position);
+    },[]);
+
     const [isDraggingLeft, setIsDraggingLeft] = useState(false);
     const [isDraggingRight, setIsDraggingRight] = useState(false);
 
@@ -28,12 +37,12 @@ export const Ruler = () => {
             if (container) {
                 const containerRect = container.getBoundingClientRect();
                 const relativeX = e.clientX - containerRect.left;
-                const rawPosition = Math.max(0, Math.min(816, relativeX));
+                const rawPosition = Math.max(0, Math.min(PAGE_WIDTH, relativeX));
 
                 if (isDraggingLeft) {
                     const maxLeftPosition = PAGE_WIDTH - rightMargin - MINIMUM_SPACE;
                     const newLeftPosition = Math.min(rawPosition, maxLeftPosition);
-                    setLeftMargin(newLeftPosition); // TODO: Make Collaborative
+                    setLeftMargin(newLeftPosition);
                 } else if (isDraggingRight) {
                     const maxRightPosition = PAGE_WIDTH - (leftMargin + MINIMUM_SPACE);
                     const newRightPosition = Math.max(PAGE_WIDTH - rawPosition, 0);
